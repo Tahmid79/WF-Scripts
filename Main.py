0fdfa6b39e3996c6bb4f39a782f849c0ecef011e
Tahmid <tahmidfarhan12@gmail.com>
2021-09-06 14:37:56 +0600

    N/A replacement

--- Main.py
+++ Main.py
@@ -1,15 +1,23 @@
 from pathlib import Path
 
-o1 = Path('output.txt').read_text()
-o2 = Path('output2.txt').read_text()
-content = Path('view.txt').read_text()
+def replace(o1 , o2, view='view.txt', result='result.txt'):
+    o1 = Path(o1).read_text()
+    o2 = Path(o2).read_text()
+    content = Path(view).read_text()
 
-o1 = o1.split("\n")
-o2 = o2.split("\n")
+    o1 = o1.split("\n")
+    o2 = o2.split("\n")
 
-for i in range(len(o1)):
-    content = content.replace(o1[i], o2[i])
+    for i in range(len(o1)):
+        content = content.replace(o1[i], o2[i])
 
-with open('result.txt', 'w') as f:
-    f.write(content)
+    with open(result, 'w') as f:
+        f.write(content)
+
+
+replace('output.txt', 'output2.txt')
+replace('NA/current.txt' , 'Na/na2.txt' , 'result.txt')
+
+
+
 
