489b9c463ae45cfea9f71f6d2ef3de92e8dc9a05
Tahmid <tahmidfarhan12@gmail.com>
2021-09-07 20:51:11 +0600

    Extract

---
+++ Extract.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+pth = 'view.txt'
+
+substring = 'ng-model="vm.formData.'
+model = []
+
+content = Path(pth).read_text()
+
+words = content.split(" ")
+
+for word in words:
+    if substring in word:
+        word = word.replace('\n' , '')
+        word = word.replace(substring, '')
+        word = word.replace('"' , '')
+        model.append(word)
+
+
+propList = ''
+
+
+for prop in model:
+    propList += prop + '\n'
+
+
+with open('properties', 'w') as f:
+    f.write(propList)
+
+
+
+
