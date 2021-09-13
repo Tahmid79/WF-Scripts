8ae3528cfb2baf6a3878ee240daf2539f16a8f97
Tahmid <tahmidfarhan12@gmail.com>
2021-09-13 18:03:17 +0600

    apply rename

--- Filename.py
+++ Filename.py
@@ -3,10 +3,13 @@ import os
 files = os.listdir('./switch-fr-charges-1st')
 
 chargesStr = 'app-process-charges-v2'
-newStr = 'app-process-charges-v2'
+newStr = 'switch-fr-charges-1st'
 
 for file in files:
     if chargesStr in file:
-        print(file)
+        newStr = file.replace(chargesStr, newStr)
+        os.rename(file, newStr)
+
+
 
 
