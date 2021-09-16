76f99a076625c5eeb7b355eeadc901a27da3f1ae
Tahmid <tahmidfarhan12@gmail.com>
2021-09-16 15:05:29 +0600

    Change lang and app name in the entire folder

--- FileContent.py
+++ FileContent.py
@@ -5,18 +5,44 @@ def WriteStringToFile(path , str):
     with open(path, 'w') as f:
         f.write(str)
 
+def ReplaceStringInAllFiles(folderPath, oldString, newString):
+    folder = os.listdir(folderPath)
+    for file in folder:
+        if newAppName in file:
+            pth = folderPath + '/' + file
+            content = Path(pth).read_text()
+            content = content.replace(oldString, newString)
+            WriteStringToFile(pth, content)
+
 oldAppName = 'app-process-charges-v2'
 newAppName = 'switch-fr-charges-1st'
 
-folderPath = './switch-fr-charges-1st'
-folder = os.listdir(folderPath)
+oldLang = 'APP_PROCESS_CHARGES_V2'
+newLang = 'SWITCH_FR_CHARGES_1ST'
 
-for file in folder:
-    if newAppName in file:
-        pth = folderPath + '/' + file
-        content = Path(pth).read_text()
-        content = content.replace(oldAppName , newAppName)
-        WriteStringToFile(pth, content)
+folderPath = './switch-fr-charges-1st'
+controllerPath = folderPath + '/controller'
+viewPath = folderPath + '/view'
+langPath = folderPath + '/i18n'
+
+# This is for replacing the app name
+ReplaceStringInAllFiles(folderPath , oldAppName , newAppName)
+ReplaceStringInAllFiles(controllerPath , oldAppName , newAppName)
+ReplaceStringInAllFiles(viewPath , oldAppName , newAppName)
+ReplaceStringInAllFiles(langPath , oldAppName , newAppName)
+
+# This is for replacing the language
+ReplaceStringInAllFiles(folderPath , oldLang , newLang)
+ReplaceStringInAllFiles(controllerPath , oldLang , newLang)
+ReplaceStringInAllFiles(viewPath , oldLang , newLang)
+ReplaceStringInAllFiles(langPath , oldLang , newLang)
+
+
+
+
+
+
+
 
 
 
