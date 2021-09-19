efce91279c3bd2d82ff771bdda4fdd0f47370f38
Tahmid <tahmidfarhan12@gmail.com>
2021-09-19 16:18:59 +0600

    refactor filename

--- Filename.py
+++ Filename.py
@@ -1,34 +1,27 @@
 import os
 
-folderStr = './switch-fr-charges-1st'
-folder = os.listdir(folderStr)
-
 chargesStr = 'app-process-charges-v2'
 newStr = 'switch-fr-charges-1st'
 
-for file in folder:
-    if chargesStr in file:
-        source = folderStr + '/' + file
-        destination = source.replace(chargesStr, newStr)
-        os.rename(source, destination)
-
+folderStr = './switch-fr-charges-1st'
 controllerStr = folderStr + '/controller'
-controllerFolder = os.listdir(controllerStr)
-
-for file in controllerFolder:
-    if chargesStr in file:
-        source = controllerStr + '/' + file
-        destination = source.replace(chargesStr , newStr)
-        os.rename(source, destination)
-
 viewStr = folderStr + '/view'
-viewFolder = os.listdir(viewStr)
 
-for file in viewFolder:
-    if chargesStr in file:
-        source =  viewStr + '/' + file
-        destination = source.replace(chargesStr, newStr)
-        os.rename(source, destination)
+
+def changeFileName(folderPath, oldname , newname):
+    folder = os.listdir(folderPath)
+    for file in folder:
+        if oldname in file:
+            source = folderStr + '/' + file
+            destination = source.replace(oldname, newname)
+            os.rename(source, destination)
+    
+
+changeFileName(folderStr, chargesStr , newStr)
+changeFileName(controllerStr,chargesStr , newStr)
+changeFileName(viewStr,chargesStr , newStr)
+
+
 
 
 
