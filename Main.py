7da7ad78a98e1e73191c59bb218eafdd2ad2495b
Tahmid <tahmidfarhan12@gmail.com>
2021-09-19 13:44:54 +0600

    get function for replacing whole words

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
+import re
 from pathlib import Path
 
-def replace(o1 , o2, view='view.txt', result='result.txt'):
+def replace(o1 , o2, view='source.txt', result='result.txt'):
     o1 = Path(o1).read_text()
     o2 = Path(o2).read_text()
     content = Path(view).read_text()
@@ -9,15 +10,16 @@ def replace(o1 , o2, view='view.txt', result='result.txt'):
     o2 = o2.split("\n")
 
     for i in range(len(o1)):
-        content = content.replace(o1[i], o2[i])
+        #  content = content.replace(o1[i], o2[i])
+        regex = r"\b" + o1[i] + r"\b"
+        content = re.sub(regex, o2[i] , content)
+
 
     with open(result, 'w') as f:
         f.write(content)
 
 
-replace('Outputs/output.txt', 'Outputs/output4.txt')
-replace('NA/current4.txt' , 'Na/na4.txt' , 'result.txt')
-
-
+replace('Outputs/output.txt', 'Outputs/output2.txt')
+# replace('NA/current.txt' , 'Na/na.txt' , 'result.txt')
 
 
