9b7d319c64b911a267118be72c460b68a2316eda
Tahmid <tahmidfarhan12@gmail.com>
2021-09-19 16:33:32 +0600

    try old function

--- Filename.py
+++ Filename.py
@@ -1,27 +1,34 @@
 import os
 
-oldname = 'app-process-charges-v2'
-newname = 'switch-fr-charges-2nd'
+folderStr = './switch-fr-charges-1st'
+folder = os.listdir(folderStr)
 
-folderStr = './switch-fr-charges-2nd'
+chargesStr = 'app-process-charges-v2'
+newStr = 'switch-fr-charges-1st'
+
+for file in folder:
+    if chargesStr in file:
+        source = folderStr + '/' + file
+        destination = source.replace(chargesStr, newStr)
+        os.rename(source, destination)
+
 controllerStr = folderStr + '/controller'
+controllerFolder = os.listdir(controllerStr)
+
+for file in controllerFolder:
+    if chargesStr in file:
+        source = controllerStr + '/' + file
+        destination = source.replace(chargesStr , newStr)
+        os.rename(source, destination)
+
 viewStr = folderStr + '/view'
+viewFolder = os.listdir(viewStr)
 
-
-def changeFileName(folderPath, oldname , newname):
-    folder = os.listdir(folderPath)
-    for file in folder:
-        if oldname in file:
-            source = folderStr + '/' + file
-            destination = source.replace(oldname, newname)
-            os.rename(source, destination)
-    
-
-changeFileName(folderStr, oldname, newname)
-changeFileName(controllerStr, oldname, newname)
-changeFileName(viewStr, oldname, newname)
-
-
+for file in viewFolder:
+    if chargesStr in file:
+        source =  viewStr + '/' + file
+        destination = source.replace(chargesStr, newStr)
+        os.rename(source, destination)
 
 
 
